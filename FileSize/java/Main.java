80a9d0962422da7c659f32500754cc7242287c56
pRusya <agat.nk@gmail.com>
2014-07-19 21:02:03 +0300

    Add File Size java solution

---
+++ FileSize/java/Main.java
@@ -0,0 +1,13 @@
+import java.io.File;
+
+public class Main {
+	public static void main(String[] args) {
+		try{
+			File file = new File(args[0]);
+			System.out.println(file.length());
+		}
+		catch(Exception e){
+			System.out.println("Error: "+e);
+		}
+	}
+}
